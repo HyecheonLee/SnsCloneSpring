3b583e83f30015b630df14738a00b75ddbe20a40
paul <paul@bamboos.co.kr>
2022-04-07 15:58:46 +0900

    #backend validation 기능 추가.

--- web/src/main/kotlin/com/hyecheon/web/api/ChatApi.kt
+++ web/src/main/kotlin/com/hyecheon/web/api/ChatApi.kt
@@ -8,6 +8,7 @@ import com.hyecheon.web.dto.web.ResponseDto
 import com.hyecheon.web.service.ChatMessageService
 import com.hyecheon.web.service.ChatService
 import org.springframework.http.MediaType
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -18,63 +19,63 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping(CHAT_V1_API)
 @RestController
 class ChatApi(
-	private val chatService: ChatService,
-	private val chatMessageService: ChatMessageService,
+    private val chatService: ChatService,
+    private val chatMessageService: ChatMessageService,
 ) {
 
-	@PostMapping("/room")
-	fun create(@RequestBody newChatRoom: ChatRoomReqDto.New) = run {
-		val chatRoom = newChatRoom.toEntity()
-		val chatRoomId = chatService.createChatRoom(chatRoom)
-		ResponseDto(data = mapOf("chatRoomId" to chatRoomId))
-	}
+    @PostMapping("/room")
+    fun create(@RequestBody @Validated newChatRoom: ChatRoomReqDto.New) = run {
+        val chatRoom = newChatRoom.toEntity()
+        val chatRoomId = chatService.createChatRoom(chatRoom)
+        ResponseDto(data = mapOf("chatRoomId" to chatRoomId))
+    }
 
-	@GetMapping("/room/{id}")
-	fun getChatRoom(@PathVariable id: Long) = run {
-		val chatRoom = chatService.findRoomWithUsersById(id)
-		ResponseDto(data = ChatRoomRespDto.toModel(chatRoom))
-	}
+    @GetMapping("/room/{id}")
+    fun getChatRoom(@PathVariable id: Long) = run {
+        val chatRoom = chatService.findRoomWithUsersById(id)
+        ResponseDto(data = ChatRoomRespDto.toModel(chatRoom))
+    }
 
-	@GetMapping("/room/{id}/message-notify", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-	fun getChatMessageNotify(@PathVariable id: Long) = run {
-		chatMessageService.getEmitter(id)
-	}
+    @GetMapping("/room/{id}/message-notify", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun getChatMessageNotify(@PathVariable id: Long) = run {
+        chatMessageService.getEmitter(id)
+    }
 
-	@GetMapping("/room/{id}/message")
-	fun getChatMessage(
-		@PathVariable id: Long,
-		@RequestParam(required = false) lastId: Long = Long.MAX_VALUE,
-	) = run {
-		val messages = chatMessageService.findAllMessage(id, lastId)
-		ResponseDto(data = messages.map(ChatMessageDto::toModel))
-	}
+    @GetMapping("/room/{id}/message")
+    fun getChatMessage(
+        @PathVariable id: Long,
+        @RequestParam(required = false) lastId: Long = Long.MAX_VALUE,
+    ) = run {
+        val messages = chatMessageService.findAllMessage(id, lastId)
+        ResponseDto(data = messages.map(ChatMessageDto::toModel))
+    }
 
-	@PostMapping("/message")
-	fun newChatMsg(@RequestBody msg: ChatMessageDto.New) = run {
-		val chatMsgId = chatMessageService.newChatMsg(msg.toEntity())
-		ResponseDto(data = mapOf("chatMessageId" to chatMsgId))
-	}
+    @PostMapping("/message")
+    fun newChatMsg(@RequestBody msg: ChatMessageDto.New) = run {
+        val chatMsgId = chatMessageService.newChatMsg(msg.toEntity())
+        ResponseDto(data = mapOf("chatMessageId" to chatMsgId))
+    }
 
-	@GetMapping("/room")
-	fun getChatRooms() = run {
-		val chatRooms = chatService.findRoomAllByLoggedUser()
-		val result = chatRooms.mapNotNull { it.chatRoom }
-			.map { ChatRoomRespDto.toModel(it) }
-			.sortedByDescending { it.updatedAt }
-		ResponseDto(data = result)
-	}
+    @GetMapping("/room")
+    fun getChatRooms() = run {
+        val chatRooms = chatService.findRoomAllByLoggedUser()
+        val result = chatRooms.mapNotNull { it.chatRoom }
+            .map { ChatRoomRespDto.toModel(it) }
+            .sortedByDescending { it.updatedAt }
+        ResponseDto(data = result)
+    }
 
-	@PatchMapping("/room/{id}")
-	fun chatRoom(@PathVariable id: Long, @RequestBody patchChatRoom: ChatRoomReqDto.Patch) = run {
-		val entity = patchChatRoom.toEntity()
-		chatService.patchRoom(id, entity)
-		ResponseDto(data = patchChatRoom)
-	}
+    @PatchMapping("/room/{id}")
+    fun chatRoom(@PathVariable id: Long, @RequestBody patchChatRoom: ChatRoomReqDto.Patch) = run {
+        val entity = patchChatRoom.toEntity()
+        chatService.patchRoom(id, entity)
+        ResponseDto(data = patchChatRoom)
+    }
 
-	@PutMapping("/room")
-	fun chatRoom(@RequestBody newChatRoom: ChatRoomReqDto.New) = run {
-		val entity = newChatRoom.toEntity()
-		val chatRoomId = chatService.saveOrFind(entity)
-		ResponseDto(data = mapOf("chatRoomId" to chatRoomId))
-	}
+    @PutMapping("/room")
+    fun chatRoom(@RequestBody newChatRoom: ChatRoomReqDto.New) = run {
+        val entity = newChatRoom.toEntity()
+        val chatRoomId = chatService.saveOrFind(entity)
+        ResponseDto(data = mapOf("chatRoomId" to chatRoomId))
+    }
 }
--- web/src/main/kotlin/com/hyecheon/web/dto/chat/ChatRoomReqDto.kt
+++ web/src/main/kotlin/com/hyecheon/web/dto/chat/ChatRoomReqDto.kt
@@ -1,6 +1,9 @@
 package com.hyecheon.web.dto.chat
 
+import com.hyecheon.web.validation.ListNotEmpty
 import org.mapstruct.factory.Mappers
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 /**
  * User: hyecheon lee
@@ -8,24 +11,27 @@ import org.mapstruct.factory.Mappers
  * Date: 2022/04/02
  */
 object ChatRoomReqDto {
-	private val converter = Mappers.getMapper(ChatConverter::class.java)
+    private val converter = Mappers.getMapper(ChatConverter::class.java)
 
-	data class Patch(
-		var chatRoomName: String? = null,
-		var userIds: List<Long>? = null,
-	) {
-		fun toEntity() = run {
-			converter.toEntity(this)
-		}
-	}
+    data class Patch(
+        var chatRoomName: String? = null,
+        var userIds: List<Long>? = null,
+    ) {
+        fun toEntity() = run {
+            converter.toEntity(this)
+        }
+    }
 
-	data class New(
-		var chatRoomName: String = "",
-		var groupChat: Boolean = false,
-		var userIds: List<Long> = listOf(),
-	) {
-		fun toEntity() = run {
-			converter.toEntity(this)
-		}
-	}
+    data class New(
+        @field:NotBlank
+        var chatRoomName: String = "",
+        var groupChat: Boolean = false,
+        @NotNull
+        @field:ListNotEmpty(message = "사용자 아이디를 입력해주세요!")
+        var userIds: List<Long> = listOf(),
+    ) {
+        fun toEntity() = run {
+            converter.toEntity(this)
+        }
+    }
 }
